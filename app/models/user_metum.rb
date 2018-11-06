5e2f13203cb2b1145379b3582d6a1081cd176fdd
Simranjeet Singh <sshunjan97@gmail.com>
2018-11-06 14:25:45 -0600

    Added the UserMeta model and its validations.

---
+++ app/models/user_metum.rb
@@ -0,0 +1,8 @@
+class UserMetum < ApplicationRecord
+
+  validates :user, :name, :address, :email, :status, presence: true
+  validates :credit_card_number, numbericality: { equal_to: 10}
+  validates :email, uniqueness: true
+
+  belongs_to :user
+end
