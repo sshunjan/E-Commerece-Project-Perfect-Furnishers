944ac7db163dfa0b1ba327f05eb74a604adcac63
Simranjeet Singh <sshunjan97@gmail.com>
2018-11-06 14:58:37 -0600

    Fixed a typo and migrated the database.

--- app/models/user_metum.rb
+++ app/models/user_metum.rb
@@ -1,7 +1,7 @@
 class UserMetum < ApplicationRecord
 
   validates :user, :name, :address, :email, :status, presence: true
-  validates :credit_card_number, numbericality: { equal_to: 10}
+  validates :credit_card_number, numericality: { equal_to: 10}
   validates :email, uniqueness: true
 
   belongs_to :user
