b8331f53b3870a02c426db094db869a7ef7d2778
Simranjeet Singh <sshunjan97@gmail.com>
2018-11-06 14:28:35 -0600

    Added the uniqueness constraint to some models.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   validates :name, :description, :category,  presence: true
   validates :price, presence: true, numericality: { greater_than: 0}
+  validates :name, uniqueness:true
 
   belongs_to :category
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   validates :username, :password, presence: true
+  validates :username, uniqueness: true
 end
