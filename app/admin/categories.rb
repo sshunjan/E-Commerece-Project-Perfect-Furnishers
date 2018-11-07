13e86675e1562a363843329c56f97c08272e0f0f
Simranjeet Singh Hunjan <simranjeetsinghhunjan@gmail.com>
2018-11-07 13:51:42 -0600

    Added a category controller and a view and other stuff.
    Well I did a lot stuff during this commit. Forgot to commit. I added a new column to the category table called icon.
    Added some more styling to the site.

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register Category do
-  permit_params :name , :description
+  permit_params :name , :description, :icon 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_06_213049) do
+ActiveRecord::Schema.define(version: 2018_11_07_190129) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2018_11_06_213049) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "icon"
   end
 
   create_table "ordered_products", force: :cascade do |t|
